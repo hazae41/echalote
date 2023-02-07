8389e98484b42f6808d16c7831de912c9020f6b7
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-02-07 15:40:26 +0100

    kcp2

--- src/mods/kcp2/segment.ts
+++ src/mods/kcp2/segment.ts
@@ -2,9 +2,14 @@ import { Binary } from "@hazae41/binary";
 
 export class Kcp2Segment {
 
+  static readonly fecTypes = {
+    data: 0xF1,
+    parity: 0xF2
+  }
+
   constructor(
     readonly nonce: Uint8Array,
-    readonly checksum: Uint8Array,
+    readonly checksum: number,
     readonly fecType: number,
     readonly fecSeqID: number,
     readonly data: Uint8Array
@@ -12,8 +17,8 @@ export class Kcp2Segment {
 
   size() {
     return 0
-      + this.nonce.length
-      + this.checksum.length
+      + 16
+      + 4
       + 4
       + 2
       + 2
@@ -21,6 +26,22 @@ export class Kcp2Segment {
   }
 
   write(binary: Binary) {
+    binary.write(this.nonce)
+    binary.writeUint32(this.checksum, true)
+    binary.writeUint32(this.fecSeqID, true)
+    binary.writeUint16(this.fecType, true)
+    binary.writeUint16(this.data.length + 2, true)
+    binary.write(this.data)
+  }
 
+  static read(binary: Binary) {
+    const nonce = binary.read(16)
+    const checksum = binary.readUint32(true)
+    const fecSeqID = binary.readUint32(true)
+    const fecType = binary.readUint16(true)
+    const length = binary.readUint16(true)
+    const data = binary.read(length - 2)
+
+    return new this(nonce, checksum, fecType, fecSeqID, data)
   }
 }
