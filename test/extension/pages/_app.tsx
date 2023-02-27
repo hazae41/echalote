bf94fe0fbe5b8a1f0fdbae1a4a73f398580a4358
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-02-27 21:29:28 +0100

    remove extension test

--- test/extension/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from 'next/app'
-import '../styles/globals.css'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <div className="w-[337.50px] h-[600px]">
-    <Component {...pageProps} />
-  </div>
-}
