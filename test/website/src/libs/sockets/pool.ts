c1a225509b446a7221e5c2bcd8504714acf30701
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-05-29 17:20:43 +0200

    socket

--- test/website/src/libs/sockets/pool.ts
+++ test/website/src/libs/sockets/pool.ts
@@ -4,10 +4,13 @@ import { Fleche } from "@hazae41/fleche"
 import { Future } from "@hazae41/future"
 import { Mutex } from "@hazae41/mutex"
 import { Pool } from "@hazae41/piscine"
+import { AbortError } from "@hazae41/plume"
 import { Ok } from "@hazae41/result"
 import { AbortSignals } from "libs/signals/signals"
 
-export async function createWebSocket(url: URL, circuit: Circuit, signal?: AbortSignal) {
+export async function createWebSocket(url: URL, circuit: Circuit, psignal?: AbortSignal) {
+  const signal = AbortSignals.timeout(5_000, psignal)
+
   const tcp = await circuit.tryOpen(url.hostname, 443, { signal }).then(r => r.unwrap())
   const tls = new TlsClientDuplex(tcp, { ciphers: [Ciphers.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384] })
   const socket = new Fleche.WebSocket(url, undefined, { subduplex: tls })
@@ -27,18 +30,30 @@ export async function createWebSocket(url: URL, circuit: Circuit, signal?: Abort
   return socket
 }
 
+export async function createWebSocketLoop(url: URL, circuits: Mutex<Pool<Circuit>>, signal?: AbortSignal) {
+  while (!signal?.aborted) {
+    try {
+      const circuit = await circuits.lock(async (circuits) => {
+        const circuit = await circuits.tryGetCryptoRandom()
+        circuit.inspectSync(circuit => circuits.delete(circuit))
+        return circuit
+      }).then(r => r.unwrap())
+
+      return await createWebSocket(url, circuit, signal)
+    } catch (e: unknown) {
+      console.warn(`WebSocket creation failed`, { e })
+      continue
+    }
+  }
+
+  throw AbortError.from(signal.reason)
+}
+
 export function createWebSocketPool(url: URL, circuits: Mutex<Pool<Circuit>>) {
   const { capacity, signal } = circuits.inner.params
 
   return new Pool<WebSocket>(async ({ pool, signal }) => {
-    const circuit = await circuits.lock(async (circuits) => {
-      const circuit = await circuits.tryGetCryptoRandom()
-      circuit.inspectSync(circuit => circuits.delete(circuit))
-      return circuit
-    }).then(r => r.unwrap())
-
-    const signal2 = AbortSignals.timeout(5_000, signal)
-    const socket = await createWebSocket(url, circuit, signal2)
+    const socket = await createWebSocketLoop(url, circuits, signal)
 
     const onCloseOrError = async () => {
       socket.removeEventListener("close", onCloseOrError)
