b8ea1dd8ad56111955b472a5fdcde75efc7a04c2
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-11-20 19:51:32 +0100

    stream reuse

--- test/website/pages/dir.tsx
+++ test/website/pages/dir.tsx
@@ -1,12 +1,12 @@
 import { Disposer } from "@hazae41/cleaner";
-import { Circuit, Echalote, Fallback, Microdesc, Microdescs, TorClientDuplex } from "@hazae41/echalote";
+import { Circuit, Echalote, Microdesc, Microdescs, TorClientDuplex } from "@hazae41/echalote";
 import { Ed25519 } from "@hazae41/ed25519";
 import { tryFetch } from "@hazae41/fleche";
 import { Mutex } from "@hazae41/mutex";
 import { Pool } from "@hazae41/piscine";
 import { Sha1 } from "@hazae41/sha1";
 import { X25519 } from "@hazae41/x25519";
-import { createTorPool2, tryCreateTor } from "libs/circuits/circuits";
+import { tryCreateTor } from "libs/circuits/circuits";
 import { DependencyList, useCallback, useEffect, useState } from "react";
 
 function useAsyncMemo<T>(factory: () => Promise<T>, deps: DependencyList) {
@@ -36,13 +36,13 @@ export default function Page() {
     // Cadenas.Console.debugging = true
   }, [])
 
-  const tors = useAsyncMemo(async () => {
-    return createTorPool2({ fallbacks: [], capacity: 3 })
-  }, [])
+  const [stream, setStream] = useState<ReadableWritablePair<any, any>>()
 
-  const onClick = useCallback(async () => {
-    try {
-      if (!tors) return
+  useEffect(() => {
+    (async () => {
+      Ed25519.set(await Ed25519.fromSafeOrBerith())
+      X25519.set(await X25519.fromSafeOrBerith())
+      Sha1.set(await Sha1.fromMorax())
 
       const start = Date.now()
 
@@ -58,9 +58,20 @@ export default function Page() {
       const microrefs = Microdescs.parseOrThrow(await response.text())
       console.log(microrefs, Date.now() - start)
 
-      const fasts = microrefs.filter(it => true
+      console.log(microrefs.flatMap(it => it.flags).filter((it, i, a) => a.indexOf(it) === i))
+
+      const seconds = microrefs.filter(it => true
         && it.flags.includes("Fast")
-        && it.flags.includes("Stable"))
+        && it.flags.includes("Stable")
+        && it.flags.includes("V2Dir"))
+
+      const thirds = microrefs.filter(it => true
+        && it.flags.includes("Fast")
+        && it.flags.includes("Stable")
+        && it.flags.includes("Exit")
+        && !it.flags.includes("BadExit"))
+
+      console.log(seconds.length, thirds.length)
 
       async function unref(ref: Microdescs.Item) {
         const start = Date.now()
@@ -78,39 +89,59 @@ export default function Page() {
       }
 
       {
-        const start = Date.now()
 
-        for (let i = 0; i < 2; i++) {
-          const ref = fasts[Math.floor(Math.random() * fasts.length)]
+        {
+          const ref = seconds[Math.floor(Math.random() * seconds.length)]
           const desc = await unref(ref)
-          console.log(ref, desc)
 
-          const fb: Fallback = {
+          await circuit.extendToOrThrow({
             id: Buffer.from(ref.identity, "base64").toString("hex"),
             onion: Buffer.from(desc.ntorOnionKey, "base64").toJSON().data,
             hosts: [`${ref.hostname}:${ref.orport}`],
             eid: desc.idEd25519
-          }
-
-          await circuit.extendToOrThrow(fb)
+          })
         }
 
-        const url = `https://twitter.com`
+        {
+          const ref = thirds[Math.floor(Math.random() * thirds.length)]
+          const desc = await unref(ref)
+
+          await circuit.extendToOrThrow({
+            id: Buffer.from(ref.identity, "base64").toString("hex"),
+            onion: Buffer.from(desc.ntorOnionKey, "base64").toJSON().data,
+            hosts: [`${ref.hostname}:${ref.orport}`],
+            eid: desc.idEd25519
+          })
+        }
+
+        const url = `https://eth.llamarpc.com`
         const stream = await circuit.openAsOrThrow(url)
 
-        const response = await tryFetch(url, { stream }).then(r => r.unwrap())
-        console.log(response, Date.now() - start)
-
-        const data = await response.text()
-        console.log(data, Date.now() - start)
+        setStream(stream)
       }
+    })()
+  }, [])
+
+  const onClick = useCallback(async () => {
+    try {
+      if (!stream) return
+
+      const start = Date.now()
+
+      const body = JSON.stringify({ "jsonrpc": "2.0", "method": "eth_blockNumber", "params": [], "id": 67 })
+      const headers = { "content-type": "application/json" }
+
+      const response = await tryFetch(`https://eth.llamarpc.com`, { stream, method: "POST", headers, body, preventClose: true, preventAbort: true, preventCancel: true }).then(r => r.unwrap())
+      console.log(response, Date.now() - start)
+
+      const data = await response.text()
+      console.log(data, Date.now() - start)
 
       console.log("Done!!!", Date.now() - start)
     } catch (e: unknown) {
       console.error("onClick", { e })
     }
-  }, [tors])
-
+  }, [stream])
 
   return <>
     <button onClick={onClick}>
