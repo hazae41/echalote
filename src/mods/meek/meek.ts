8e663ea5d6a73f6b9e47b6711435de795083c6dd
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-11-22 16:01:22 +0100

    clean

--- src/mods/meek/meek.ts
+++ src/mods/meek/meek.ts
@@ -1,11 +1,8 @@
-import { Cadenas } from "@hazae41/cadenas"
 import { BatchedFetchStream } from "libs/transports/http.js"
 
 export async function createMeekStream(url: string) {
   const headers = { "x-session-id": crypto.randomUUID() }
   const request = new Request(url, { headers })
-
-  Cadenas.Console.debugging = true
 
   return new BatchedFetchStream(request)
 }
