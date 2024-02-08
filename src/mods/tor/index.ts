cf1e965d5f8c88d7df056654fdcc7c75208ce819
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-02-08 14:34:20 +0100

    clean

--- src/mods/tor/index.ts
+++ src/mods/tor/index.ts
@@ -6,7 +6,6 @@ export * from "./circuit.js";
 export * from "./consensus/index.js";
 export * from "./constants.js";
 export * from "./errors.js";
-export * from "./pool.js";
 export * from "./state.js";
 export * from "./stream.js";
 export * from "./target.js";
