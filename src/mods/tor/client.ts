739917e0ffd1d74d6a07b4e75af64936daf91299
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-03-27 23:30:48 +0100

    fix log

--- src/mods/tor/client.ts
+++ src/mods/tor/client.ts
@@ -4,7 +4,6 @@ import { Bytes, Uint8Array } from "@hazae41/bytes";
 import { Ciphers, TlsClientDuplex } from "@hazae41/cadenas";
 import { HalfDuplex } from "@hazae41/cascade";
 import { Cursor } from "@hazae41/cursor";
-import { Ed25519 } from "@hazae41/ed25519";
 import { Future } from "@hazae41/future";
 import { Mutex } from "@hazae41/mutex";
 import { None } from "@hazae41/option";
@@ -183,7 +182,6 @@ export class SecretTorClientDuplex {
   }
 
   async #init() {
-    console.log(Ed25519)
     await Paimon.initBundledOnce()
     await Zepar.initBundledOnce()
   }
